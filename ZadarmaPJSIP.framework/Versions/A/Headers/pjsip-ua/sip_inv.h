43c8577d6e714a7f12b1e4e1621c0fbb75f54a6e
Marina Zayceva <m.zayceva@sip-cloud.net>
2021-12-17 13:45:15 +0500

    PJSIP 10

--- ZadarmaPJSIP.framework/Versions/A/Headers/pjsip-ua/sip_inv.h
+++ ZadarmaPJSIP.framework/Versions/A/Headers/pjsip-ua/sip_inv.h
@@ -131,8 +131,7 @@ typedef struct pjsip_inv_callback
      * This callback is called when the invite usage module has created 
      * a new dialog and invite because of forked outgoing request.
      *
-     * Currently the invite session does not create a new dialog in
-     * forking scenario, so this callback will never be invoked.
+     * This callback is mandatory.
      *
      * @param inv	The new invite session.
      * @param e		The event which has caused the dialog to fork.
@@ -256,8 +255,7 @@ typedef struct pjsip_inv_callback
      * sending of the ACK request (for example, when it needs to 
      * wait for answer from the other call leg, in 3PCC scenarios). 
      *
-     * Application MUST create the ACK request using pjsip_inv_create_ack()
-     * and send it using pjsip_inv_send_msg().
+     * Application creates the ACK request
      *
      * Once it has sent the ACK request, the framework will keep 
      * this ACK request in the cache. Subsequent receipt of 2xx response
@@ -377,17 +375,7 @@ enum pjsip_inv_option
      * Session timer extension will always be used even when peer doesn't
      * support/want session timer.
      */
-    PJSIP_INV_ALWAYS_USE_TIMER	= 128,
-
-    /**
-     * Indicate support for trickle ICE
-     */
-    PJSIP_INV_SUPPORT_TRICKLE_ICE = 256,
-
-    /**
-     * Require trickle ICE support.
-     */
-    PJSIP_INV_REQUIRE_TRICKLE_ICE = 512,
+    PJSIP_INV_ALWAYS_USE_TIMER	= 128
 
 };
 
@@ -977,7 +965,7 @@ PJ_DECL(pj_status_t) pjsip_inv_update (	pjsip_inv_session *inv,
  *
  * Note that if the invite session has a pending offer to be answered 
  * (for example when the last 2xx response to INVITE contains an offer), 
- * application MUST have set the SDP answer with #pjsip_inv_set_sdp_answer()
+ * application MUST have set the SDP answer with #pjsip_create_sdp_body()
  * prior to creating the ACK request. In this case, the ACK request
  * will be added with SDP message body.
  *
@@ -1071,25 +1059,6 @@ PJ_DECL(pj_status_t) pjsip_create_sdp_body(pj_pool_t *pool,
  * @return		The SDP info.
  */
 PJ_DECL(pjsip_rdata_sdp_info*) pjsip_rdata_get_sdp_info(pjsip_rx_data *rdata);
-
-
-/**
- * Retrieve SDP information from an incoming message. Application should
- * prefer to use this function rather than parsing the SDP manually since
- * this function supports multipart message body.
- *
- * This function will only parse the SDP once, the first time it is called
- * on the same message. Subsequent call on the same message will just pick
- * up the already parsed SDP from the message.
- *
- * @param rdata		The incoming message.
- * @param med_type	The SDP media type.
- *
- * @return		The SDP info.
- */
-PJ_DECL(pjsip_rdata_sdp_info*) pjsip_rdata_get_sdp_info2(
-					    pjsip_rx_data *rdata,
-					    const pjsip_media_type *med_type);
 
 
 PJ_END_DECL
