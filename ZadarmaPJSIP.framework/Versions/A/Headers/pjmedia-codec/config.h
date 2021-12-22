c6cc079fa92414f7cce22f0c2948d2b507ac164e
Marina Zayceva <m.zayceva@sip-cloud.net>
2021-12-22 18:58:46 +0500

    2.10

--- ZadarmaPJSIP.framework/Versions/A/Headers/pjmedia-codec/config.h
+++ ZadarmaPJSIP.framework/Versions/A/Headers/pjmedia-codec/config.h
@@ -571,6 +571,35 @@
 #endif
 
 /**
+ * Determine the log level of the native openH264 log which will be forwarded
+ * to the library's log.
+ * Set to WELS_LOG_QUIET to disable logging, or WELS_LOG_DETAIL for debugging.
+ *
+ * Default: WELS_LOG_ERROR.
+ */
+#ifndef PJMEDIA_CODEC_OPENH264_LOG_LEVEL
+#   define PJMEDIA_CODEC_OPENH264_LOG_LEVEL  WELS_LOG_ERROR
+#endif
+
+/**
+ * Enable VPX VP8 codec.
+ *
+ * Default: 1
+ */
+#ifndef PJMEDIA_HAS_VPX_CODEC_VP8
+#   define PJMEDIA_HAS_VPX_CODEC_VP8		1
+#endif
+
+/**
+ * Enable VPX VP9 codec.
+ *
+ * Default: 0 (disabled)
+ */
+#ifndef PJMEDIA_HAS_VPX_CODEC_VP9
+#   define PJMEDIA_HAS_VPX_CODEC_VP9		0
+#endif
+
+/**
  * @}
  */
 
